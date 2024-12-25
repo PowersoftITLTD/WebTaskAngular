d4c20012ef1788065afeb2d76405c8b64d2d0b10
Hites1 <hitesh.ghadage@powersoft.in>
2024-12-25 12:42:03 +0530

    commit 25-12-2024
    commit 25-12-2024

--- src/app/directives/alphabet-only/alphabetic-only.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AlphabeticOnlyDirective } from './alphabetic-only.directive';
-
-describe('AlphabeticOnlyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AlphabeticOnlyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/side-bar-directive/side-bar.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SideBarDirective } from './side-bar.directive';
-
-describe('SideBarDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SideBarDirective();
-    expect(directive).toBeTruthy();
-  });
-});
